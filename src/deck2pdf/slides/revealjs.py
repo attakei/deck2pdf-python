4f55c02c477e824629dcab0e8c34e5038d337935
Kazuya Takei <myself@attakei.net>
2024-10-10 01:45:23 +0900

    refactor: Add note for viewport of revealjs

--- src/deck2pdf/slides/revealjs.py
+++ src/deck2pdf/slides/revealjs.py
@@ -8,6 +8,8 @@ class SlideReader(SlideReaderBase):
         """Procedure before capture."""
         # NOTE: It may be should do as optional.
         self._page.evaluate("Reveal.configure({progress: false});")
+        # Reveal.js can set viewport from config.
+        # It catches this as "best presentation size".
         if self._page.viewport_size:
             self._size = self._page.viewport_size
 
