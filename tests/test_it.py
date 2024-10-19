780c6790a4bcc0b8e1c64ce2f961b1390b87779a
Kazuya Takei <myself@attakei.net>
2024-10-19 10:59:46 +0900

    ci: Add e2e test (only to check work correctly with presentation URL)

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,2 +1,13 @@
-def test_dummy():
-    assert True is True
+from pathlib import Path
+
+from click.testing import CliRunner
+from deck2pdf.cli import main
+
+
+def test_as_simple_e2e(tmp_path: Path):
+    url = "https://slides.attakei.net/pyconjp-2022/"
+    dest = tmp_path / "output.pdf"
+    runner = CliRunner()
+    result = runner.invoke(main, [url, str(dest)])
+    assert result.exit_code == 0
+    assert dest.exists()
